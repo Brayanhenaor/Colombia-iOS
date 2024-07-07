de26a89434866d82e455dbef7ae90035be8feb02
Brayan Henao <bhenao@enlace.org>
2024-07-06 19:09:16 -0500

    Initial Commit

---
+++ colombia/colombiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  colombiaApp.swift
+//  colombia
+//
+//  Created by Brayan Henao on 6/07/24.
+//
+
+import SwiftUI
+
+@main
+struct colombiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
